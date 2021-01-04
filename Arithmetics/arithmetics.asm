72afc3fc2e1a7fb0ffa3a841112311ee5f1e1b42
Rafiki <roigarf@gmail.com>
2021-01-04 15:18:09 +0200

    added sum function

--- Arithmetics/arithmetics.asm
+++ Arithmetics/arithmetics.asm
@@ -10,30 +10,77 @@ org 100h
 
 jmp main
 
-vec1 db 1, 2, 5, 6
-vec2 db 3, 5, 6, 1
-vec3 db ?, ?, ?, ?
+DATA:
 
-start:
+first_arg   dw 14
+second_arg  dw -10
 
-lea si, vec1
-lea bx, vec2
-lea di, vec3
 
-mov cx, 4
 
+CODE:
+
+main:
+
+push    first_arg
+push    second_arg
+call    sum
+add     sp, 4
+
+
+
+int     21h
+
+;; --------------------------------------------------------------------
+;;                              Functions:  
+;; --------------------------------------------------------------------
+
+
+
+;; --------------------------------------------------------------------
+;; function name:   add2
+;; purpose:         implement addition
+;; input:
+;;      - first_argument:   first argument for the addition
+;;      - second_argument:  second argument for addition
+;; --------------------------------------------------------------------
 sum:       
-    mov al, [si]
-    add al, [bx]
-    mov [di], al
-   
-    inc si
-    inc bx
-    inc di
-   
-    loop sum
+    ;; stack initialization:
+    push    bp
+    mov     bp, sp       
+    
+    ;; register preservation:
+    push    cx
+    push    bx
+    
+    mov     cx, [bp + 6]
+    mov     bx, [bp + 4]
+    
+    cmp     cx, 0
+    je      end
+    
+    mov     ax, cx
+    
+    and     cx, bx
+    shl     cx ,1
+    xor     ax, bx
+    
+    push    cx
+    push    ax
+    call    sum
+    add     sp, 4
+    
+    
+    end:
+    pop     bx
+    pop     cx
+    
+    
+    ;; stack restoration:
+    mov sp, bp
+    pop bp
+    ret
 
-ret
+
 
 
 
