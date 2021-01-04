eb48dfa3ac36993a2b1f73e4b1b0d613adeb12d3
froykeZBM <roigarf@gmail.com>
2021-01-04 17:04:49 +0200

    addaed template for multiply function

--- Arithmetics/arithmetics.asm
+++ Arithmetics/arithmetics.asm
@@ -13,7 +13,7 @@ jmp main
 DATA:
 
 first_arg   dw 14
-second_arg  dw -10
+second_arg  dw 10
 
 
 
@@ -24,6 +24,7 @@ main:
 push    first_arg
 push    second_arg
 call    sum
+call    sub2
 add     sp, 4
 
 
@@ -55,16 +56,18 @@ sum:
     mov     cx, [bp + 6]
     mov     bx, [bp + 4]
     
-     
+    ;; if cx is 0 nothing to add   (this is alsso stopping conditiob for recursion)
     cmp     cx, 0
     je      end
     
     mov     ax, cx
     
-    and     cx, bx
-    shl     cx ,1
-    xor     ax, bx
+    and     cx, bx 
+    shl     cx ,1  ;; shifted and is the carry on
+    xor     ax, bx ;; xor is the addition without carry on
     
+    
+    ;; add the carry on and the addition without carry on
     push    cx
     push    ax
     call    sum
@@ -81,6 +84,67 @@ sum:
     pop bp
     ret
 
+
+
+
+;; --------------------------------------------------------------------
+;; function name:   sub2
+;; purpose:         implement subtraction
+;; input:
+;;      - first_argument:   first argument for the sub function
+;;      - second_argument:  second argument for sub function
+;; --------------------------------------------------------------------
+sub2:       
+    ;; stack initialization:
+    push    bp
+    mov     bp, sp       
+    
+    ;; register preservation
+    push    bx
+    push    cx
+    
+    mov     bx, [bp + 6] ; first argument
+    mov     cx, [bp + 4] ; second argument
+    
+    neg     cx
+    
+    push    bx
+    push    cx
+    call    sum
+    add     sp, 4
+    
+    
+    ;;register preservation
+    pop     cx
+    pop     bx
+    
+    
+    
+    ;; stack restoration:
+    mov sp, bp
+    pop bp
+    ret
+
+ ;; --------------------------------------------------------------------
+;; function name:   mul2
+;; purpose:         implement multiply
+;; input:
+;;      - first_argument:   first argument for the mul2 function
+;;      - second_argument:  second argument for mul2 function
+;; --------------------------------------------------------------------
+sub2:       
+    ;; stack initialization:
+    push    bp
+    mov     bp, sp       
+    
+    
+    
+    
+    
+    ;; stack restoration:
+    mov sp, bp
+    pop bp
+    ret
 
 
 
